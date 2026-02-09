f77f10cdc01f4e5b3fa481693937666247b7b154
Adams237 <adamoun505@gmail.com>
2026-02-09 09:46:07 +0100

    update api

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://51.75.123.172:5005';
 
 export interface ObjectData {
     _id?: string;
